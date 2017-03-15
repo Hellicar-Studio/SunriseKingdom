17709516b550f0104ad1e9551ebfee1bcc9f0188
Jason Walters <hello@glitchbeam.com>
2017-03-15 19:45:10 +0100

    initial push

---
+++ SunriseKingdom/Assets/Scripts/ImageSequence.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ImageSequence : MonoBehaviour {
+
+    public string loadFromFolder = "Images";
+    public float framesPerSecond = 10.0f;
+
+    private Renderer rend;
+    private Texture2D[] frames;
+    private string[] files;
+    private int frame = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // reference components
+        rend = GetComponent<Renderer>();
+        // get file paths
+        files = Directory.GetFiles(loadFromFolder, "*.png");
+        // process images into a sequence
+        StartCoroutine(LoadImages());
+	}
+
+    private IEnumerator LoadImages()
+    {
+        //load all images in folder as textures
+        frames = new Texture2D[files.Length];
+
+        frame = 0;
+        Texture2D texTmp = new Texture2D(1424, 220, TextureFormat.RGB24, false);
+        texTmp.filterMode = FilterMode.Point;
+
+        foreach (string tstring in files)
+        {
+            string pathTemp = "file://" + Application.dataPath + "/../" + tstring;
+            WWW www = new WWW(pathTemp);
+            yield return www;
+            www.LoadImageIntoTexture(texTmp);
+
+            frames[frame] = texTmp;
+            frames[frame].filterMode = FilterMode.Point;
+
+            rend.material.SetTexture("_MainTex", texTmp);
+            frame++;
+        }
+    }
+}
---
+++ SunriseKingdom/Assets/Scripts/WWWGetRequest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+using System;
+
+public class WWWGetRequest : MonoBehaviour {
+
+    public static bool sunriseActive;
+
+    public string URL = "http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=7f09e7d718a5c1dd8d39f1635ac7f006";
+
+    private string unixTime;
+    private string data;
+    private int utcTime;
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    void Update()
+    {
+        if (DateTime.UtcNow.ToLocalTime().ToShortTimeString() == GetSunriseTime(utcTime))
+        {
+            if (!sunriseActive) sunriseActive = true;
+        }
+    }
+
+    IEnumerator GetText() 
+    {
+        using(UnityWebRequest www = UnityWebRequest.Get(URL))
+        {
+            yield return www.Send();
+
+            if(www.isError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // results as text
+                data = www.downloadHandler.text;
+                // parses text based on http://openweathermap.org/api for JSON
+                JObject j = JObject.Parse(data);
+                utcTime = (int)j.GetValue("sys").SelectToken("sunrise");
+                // display results in the console
+                Debug.Log(GetSunriseTime(utcTime));
+            }
+        }
+    }
+
+    // gets sunrise time and converts UNIX/UTC to local time && short time (removes the date)
+    public string GetSunriseTime(int _time)
+    {
+        string t;
+        t = string.Format("{0:d/M/yyyy HH:mm:ss}", new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(_time).ToLocalTime().ToShortTimeString());
+
+        return t;
+    }
+}
