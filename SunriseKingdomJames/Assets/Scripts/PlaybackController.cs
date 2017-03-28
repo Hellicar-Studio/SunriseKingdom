94f396bbbcf772040dd3c3786cde0a6f7b9e12d8
Bentleyj <jgbentley10@gmail.com>
2017-03-28 19:29:56 +0100

    Added Internal Camera Recording control

---
+++ SunriseKingdomJames/Assets/Scripts/PlaybackController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaybackController : MonoBehaviour {
+
+    RenderHeads.Media.AVProVideo.MediaPlayer player;
+
+    public bool swapVideo;
+    public string vid1;
+    public string vid2;
+	// Use this for initialization
+	void Start () {
+        if (player == null)
+            player = FindObjectOfType<RenderHeads.Media.AVProVideo.MediaPlayer>();
+
+        player.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
