81c6f62efcb6c0cc3da111215bed41a0b85c66fe
Bentleyj <jgbentley10@gmail.com>
2017-04-03 21:52:22 +0100

    Added new control method with NAS controller much simpler!

--- SunriseKingdomJames/Assets/Scripts/PlaybackController.cs
+++ SunriseKingdomJames/Assets/Scripts/PlaybackController.cs
@@ -4,104 +4,30 @@ using System.Collections;
 
 public class PlaybackController : MonoBehaviour {
 
-    public MediaPlayer currentPlayer;
-    public MediaPlayer nextPlayer;
+    public MediaPlayer player;
 
-    public ApplyToMesh videoCanvas;
+    public CameraController cameraController;
 
-    public string vid1;
-    public string vid2;
+    string lastVideo;
 
-    public bool swapButton;
-    public bool loadButton;
-    public bool initialLoadButton;
-    public bool playButton;
-
-    public float nextVidIndex;
-    public float totalVideos;
+    string rootFolder = "D:\\SunriseNAS";
 
 	// Use this for initialization
 	void Start () {
-        if (currentPlayer == null)
-            currentPlayer = FindObjectOfType<MediaPlayer>();
-
-        if (nextPlayer == null)
-            nextPlayer = FindObjectOfType<MediaPlayer>();
-
-        if (videoCanvas == null)
-            videoCanvas = FindObjectOfType<ApplyToMesh>();
-
-        loadInitialVideos();
-
-        nextVidIndex = 2;
+        lastVideo = cameraController.getRecordingPath(rootFolder);
+        if (lastVideo != "")
+        {
+            player.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, lastVideo, true);
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        videoCanvas._media = currentPlayer;
-        if(swapButton)
+        if(lastVideo != cameraController.getRecordingPath(rootFolder))
         {
-            swapButton = false;
-            swapVideo();
+            lastVideo = cameraController.getRecordingPath(rootFolder);
+            player.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, lastVideo, true);
         }
-        if(loadButton)
-        {
-            loadButton = false;
-            loadVideoInNextPlayer();
-        }
-        if(playButton)
-        {
-            playButton = false;
-            if(!currentPlayer.Control.IsPlaying()) 
-                currentPlayer.Play();
-        }
-        if (initialLoadButton)
-        {
-            initialLoadButton = false;
-            loadInitialVideos();
-        }
-        if (currentPlayer.Control.IsFinished())
-        {
-            swapVideo();
-            //loadVideoInNextPlayer();
-        }
-    }
 
-    void swapVideo()
-    {
-        MediaPlayer tmp;
-        tmp = currentPlayer;
-        currentPlayer = nextPlayer;
-        nextPlayer = tmp;
-        currentPlayer.Play();
-    }
-
-    void loadVideoInNextPlayer()
-    {
-        nextPlayer.CloseVideo();
-        nextPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, vid2, false);
-        advanceVideoStrings();
-    }
-
-    void advanceVideoStrings()
-    {
-        nextVidIndex++;
-        nextVidIndex %= totalVideos;
-        vid1 = vid2;
-        vid2 = "Videos/Test" + nextVidIndex.ToString() + ".mkv";
-    }
-
-    void loadInitialVideos()
-    {
-        currentPlayer.CloseVideo();
-        nextPlayer.CloseVideo();
-
-        currentPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, vid1, false);
-        nextPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, vid2, false);
-    }
-
-    void PlayCurrentPlayer()
-    {
-        currentPlayer.Play();
     }
 }
