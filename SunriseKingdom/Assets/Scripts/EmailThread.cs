09b5853f72c902d49eac48e3bd805d198d6c7d78
jasonrwalters <hello@glitchbeam.com>
2017-04-06 09:52:28 +0200

    - added threaded email w/ attachments

--- SunriseKingdom/Assets/Scripts/EmailThread.cs
+++ SunriseKingdom/Assets/Scripts/EmailThread.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
+using System.Threading;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-public class ImageEmailer : MonoBehaviour
+public class EmailThread : MonoBehaviour
 {
+    #region Public data
     public string fromAddress = "from@email.here";
     public string toAddress = "to@email.here";
     public string subject = "Image from Sunrise Kingdom";
@@ -15,19 +16,56 @@ public class ImageEmailer : MonoBehaviour
     public string password = "";
     public string SMTPServer = "smtp.gmail.com";
     public int SMTPPort = 587;
-
     public bool emailSent = false;
     public bool debugActive = false;
+    #endregion
+
+    #region Private data
+    private Thread _t1;
+    private bool _t1Paused = false;
+    private Mutex _mutex = new Mutex();
+    #endregion
+
+    #region Start
+    void Start()
+    {
+        _t1 = new Thread(_func1);
+    }
+    #endregion
+
+    #region Threads
+    private void _func1()
+    {
+        while (emailSent)
+        {
+            _mutex.WaitOne();
+            SendEmail();
+            _mutex.ReleaseMutex();
+
+            do
+            {
+                Thread.Sleep(200);
+            }
+            while (_t1Paused);
+            
+        }
+    }
 
     void Update()
     {
         if (emailSent)
         {
-            SendEmail();
+            if (!_t1.IsAlive)
+            {
+                _t1.Start();
+            }
+            else
+                _t1Paused = !_t1Paused;
         }
     }
+    #endregion
 
-    public void SendEmail()
+    private void SendEmail()
     {
         MailMessage mail = new MailMessage();
 
@@ -51,7 +89,8 @@ public class ImageEmailer : MonoBehaviour
 
         if (debugActive)
             Debug.Log("Email has been successfully sent!");
-
+        
+        _t1Paused = true;
         emailSent = false;
     }
 }
